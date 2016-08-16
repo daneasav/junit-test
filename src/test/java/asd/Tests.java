ccf20142481ccba30844d3bc0256f8e58e49ea2d
despickme <victor.daneasa@haufe-lexware.com>
2016-08-16 18:15:48 +0300

    first

---
+++ src/test/java/asd/Tests.java
@@ -0,0 +1,17 @@
+package asd;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Created by DaneasaV on 16.08.2016.
+ */
+public class Tests {
+
+  @Test
+  public void multiplicationOfZeroIntegersShouldReturnZero() {
+    assertEquals("10 x 0 must be 0", 0, 10 * 0);
+  }
+
+}
